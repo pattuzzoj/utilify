8a8b879cae45fc7bb7756266cd3acd19141ffbd5
pattuzzoj <pattuzzoj@users.noreply.github.com>
2025-07-23 11:00:15 +0000

    deploy: ae8170caeddeb15705ee64348508cc46929b1365

---
+++ assets/license.md.CbLDYqc1.lean.js
@@ -0,0 +1,22 @@
+import{_ as t,c as n,j as o,o as i}from"./chunks/framework.DWWPIHUH.js";const I=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home"},"headers":[],"relativePath":"license.md","filePath":"license.md","lastUpdated":1753268366000}'),s={name:"license.md"};function a(r,e,T,c,l,O){return i(),n("div",null,e[0]||(e[0]=[o("pre",{style:{"margin-top":"100px",display:"flex","justify-content":"justify-center","align-items":"center","flex-direction":"column",width:"100%"}},`    MIT License
+
+    Copyright (c) 2025 Júlio Pattuzzo
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+`,-1)]))}const R=t(s,[["render",a]]);export{I as __pageData,R as default};
